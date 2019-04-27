92196634d32532c5a8362688edc31f51750be9e7
Ilian Mitev <Ilian.Mitev@Jagex.com>
2019-04-27 17:35:54 +0100

    added sensor input and models

--- dispensibility_server/dispensibility_server/views.py
+++ dispensibility_server/dispensibility_server/views.py
@@ -1,8 +1,20 @@
 from dispensibility_server import app, db
 from dispensibility_server.models import *
 from flask import render_template, url_for, abort, request, redirect
-
+import json
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/api/sensor_input', methods=['POST'])
+def sensor_input(input):
+    input_json = json.loads(input)
+    weight = input_json.get('weight',None)
+    timestamp = input_json.get('timestamp',None)
+
+    assert type(weight).__name__ == 'int', "Weight not integer or is None"
+    assert type(timestamp).__name__ == 'str', "Timestamp not integer or is None"
+
+    # TODO: save to db
+
