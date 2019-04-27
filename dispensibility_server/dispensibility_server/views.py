3dbc04c9cb4dd8050a95488d014cccf260bdf6d1
Jashan Bhoora <jb1352@york.ac.uk>
2019-04-27 17:35:51 +0100

    Added myitems mockup

--- dispensibility_server/dispensibility_server/views.py
+++ dispensibility_server/dispensibility_server/views.py
@@ -6,3 +6,7 @@ from flask import render_template, url_for, abort, request, redirect
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/myitems")
+def myitems():
+    return render_template("myitems.html")
